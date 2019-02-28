413cce8fbab720c3ccf89fa3d06f455d51de532b
Sujoy Ghosal <sujoy.ghosal@gmail.com>
2019-02-28 16:02:36 -0500

    Added Webhook

--- server.js
+++ server.js
@@ -16,6 +16,18 @@ var personalityInsights = new PersonalityInsightsV3({
     url: 'https://gateway.watsonplatform.net/personality-insights/api'
 });
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
+};
+app.use(allowCrossDomain);
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
