a872315ea3ecb610d215360d35a3712d87c3af9d
Sujoy Ghosal <sujoy.ghosal@gmail.com>
2019-02-28 15:49:02 -0500

    Added Webhook

--- gitpush.sh
+++ gitpush.sh
@@ -1,2 +1,4 @@
-git remote add origin https://github.com/sujoyghosal/personality-analyser.git
+#Add following line if repository is existing
+#git remote add origin https://github.com/sujoyghosal/personality-analyser.git
+git commit -m $1
 git push -u origin master
