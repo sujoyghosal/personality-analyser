0b8998b835a5aeb617d0629aa3e491d3690b1da8
Sujoy Ghosal <sujoy.ghosal@gmail.com>
2019-03-01 11:01:28 -0500

    minor change

--- tests/app_test.js
+++ tests/app_test.js
@@ -1,7 +1,7 @@
-var server   = require('../server'),
-    chai     = require('chai'),
+var server = require('../server'),
+    chai = require('chai'),
     chaiHTTP = require('chai-http'),
-    should   = chai.should();
+    should = chai.should();
 
 chai.use(chaiHTTP);
 
@@ -9,23 +9,23 @@ reqServer = process.env.HTTP_TEST_SERVER || server
 
 describe('Basic routes tests', function() {
 
-    it('GET to / should return 200', function(done){
+    it('GET to / should return 200', function(done) {
         chai.request(reqServer)
-        .get('/')
-        .end(function(err, res) {
-            res.should.have.status(200);
-            done();
-        })
+            .get('/')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                done();
+            })
 
     })
 
-    it('GET to /pagecount should return 200', function(done){
+    it('GET to /recordscount should return 200', function(done) {
         chai.request(reqServer)
-        .get('/pagecount')
-        .end(function(err, res) {
-            res.should.have.status(200);
-            done();
-        })
+            .get('/recordscount')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                done();
+            })
 
     })
 })
